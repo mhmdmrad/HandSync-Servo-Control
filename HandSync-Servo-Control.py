2d544c0ab73594e33ec4b04a085c0b5f978651ad
Mohamad Mrad <m7amadmr4d@gmail.com>
2025-06-08 21:39:10 +0300

    Update HandSync-Servo-Control.py

--- HandSync-Servo-Control.py
+++ HandSync-Servo-Control.py
@@ -16,7 +16,7 @@ time.sleep(1)
 mp_hands = mp.solutions.hands
 mp_draw = mp.solutions.drawing_utils
 hands = mp_hands.Hands(
-    max_num_hands=2,  # Detect up to 2 hands
+    max_num_hands=2,  # Detect 2 hands
     min_detection_confidence=0.7,
     min_tracking_confidence=0.7
 )
